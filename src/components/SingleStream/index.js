215ec4a8b917e4d9f8255d21a6bcdc6605672b92
Anders Blom <anders@andersblom.dk>
2017-11-20 08:34:30 -0500

    SingleStream: changed hover effect to be controlled with state in stead, for easier css maintenance

--- src/components/SingleStream/index.js
+++ src/components/SingleStream/index.js
@@ -1,38 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import './singlestream.css';
 
-const SingleStream = ({ stream }) => {
-    return(
-        <Link to={`/watch/${stream.channel.name}`} className="singleStream__container">
-            <div className="singleStream__container--hover">
-                Live for {getLiveTime(stream.created_at)}
-            </div>
-            <div className="singleStream__backgroundImage" style={{ backgroundImage: `url(${stream.preview.large})` }}></div>
-            <div className="singleStream__info">
-                <div className="singleStream__info__name">{stream.channel.name}</div>
-                <div className="singleStream__info__game">playing {stream.channel.game}</div>
-                <div className="singleStream__info__viewers">
-                    <i className="fa fa-users" aria-hidden="true"></i>
-                    <div className="text">{stream.viewers.toLocaleString()}</div>
+export default class SingleStream extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hover: false,
+        }
+    }
+
+    getLiveTime(streamCreationDate) {
+        let now = new Date(Date.now());
+        let startedAt = new Date(streamCreationDate);
+        let calculatedLiveTime = Math.floor(((now - startedAt) / 1000) / 60 / 60);
+        return `${calculatedLiveTime} hours`;
+    }
+
+    toggleHover() {
+        this.setState({
+            hover: !this.state.hover
+        });
+    }
+
+    render() {
+        const { stream } = this.props;
+        return(
+            <Link to={`/watch/${stream.channel.name}`} className={"singleStream__container " + (this.state.hover ? "singleStream__container--hover" : "")} onMouseEnter={() => this.toggleHover()} onMouseLeave={() => this.toggleHover()}>
+                <div className="singleStream__container__hovertext">
+                    Live for {this.getLiveTime(stream.created_at)}
                 </div>
-            </div>
-        </Link>
-    );
+                <div className="singleStream__backgroundImage" style={{ backgroundImage: `url(${stream.preview.large})` }}></div>
+                <div className="singleStream__info">
+                    <div className="singleStream__info__name">{stream.channel.name}</div>
+                    <div className="singleStream__info__game">playing {stream.channel.game}</div>
+                    <div className="singleStream__info__viewers">
+                        <i className="fa fa-users" aria-hidden="true"></i>
+                        <div className="text">{stream.viewers.toLocaleString()}</div>
+                    </div>
+                </div>
+            </Link>
+        );
+    }
 }
-
-SingleStream.propTypes = {
-    stream: PropTypes.object.isRequired,
-    i: PropTypes.number.isRequired
-}
-
-function getLiveTime(streamCreationDate) {
-    let now = new Date(Date.now());
-    let startedAt = new Date(streamCreationDate);
-    let calculatedLiveTime = Math.floor(((now - startedAt) / 1000) / 60 / 60);
-    return `${calculatedLiveTime} hours`;
-}
-
-export default SingleStream;
