16108917b626fd8f5c51a20ad02d032f119d543b
Anders Blom <anders@andersblom.dk>
2017-11-21 15:56:20 -0500

    border radius matters

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,6 +11,7 @@ const Header = () => (
         <nav>
             <Link to="/" className="header__nav__link"><i className="fa fa-bolt" aria-hidden="true"></i></Link>
             <Link to="/games" className="header__nav__link"><i className="fa fa-gamepad" aria-hidden="true"></i></Link>
+            <Link to="/user" className="header__nav__link--user"><img src="https://static-cdn.jtvnw.net/user-default-pictures/cd618d3e-f14d-4960-b7cf-094231b04735-profile_image-70x70.jpg" /></Link>
         </nav>
     </header>
 );
