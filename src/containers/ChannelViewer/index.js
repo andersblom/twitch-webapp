85aa6b139d759c68d26f3cfe4b7adaaefdb613a4
Anders Blom <anders@andersblom.dk>
2017-11-22 16:52:27 -0500

    Commenting for clarity in ChannelViewer container

--- src/containers/ChannelViewer/index.js
+++ src/containers/ChannelViewer/index.js
@@ -3,23 +3,36 @@ import axios from 'axios';
 
 import Loading from '../../components/Loading';
 
-// https://api.twitch.tv/kraken/streams/${channel}/?client_id=8idy5f7ryf7c84459was7n8bpcjqzc
-
 export default class ChannelViewer extends Component {
     componentDidMount() {
         const channel = this.props.match.params.streamerName;
         this.getStreamByChannelName(channel);
     }
-
+    
+    // Gets initiated first. If channel is offline: runs getOfflineStreamByChannelName()
     getStreamByChannelName(channel) {
         axios.get(`https://api.twitch.tv/kraken/streams/${channel}/?client_id=${process.env.REACT_APP_TWITCH_CLIENT_ID}`)
         .then(res => {
-            if (res.data.stream === null) {
+            // For offline channels, API returns stream: null
+            if (res.data.stream === null) { 
+                // Calls apropriate end-point for offline channels
                 this.getOfflineStreamByChannelName(channel);
-            } else {
+            
+            // Channel is online
+            } else { 
                 const { stream } = res.data;
+                /*
+                ** Creating a data object:
+                ** stream: Obj
+                ** channel: Obj
+                */
                 const OnlineChannel = Object.assign({}, { stream: stream, channel: stream.channel });
+                // { channel } is automatically nested within stream, so removing this
+                // to ensure that objects of offline/online channels look the same 
+                // (so the reducer can stay a pure function)
                 delete OnlineChannel.stream.channel;
+                
+                // Finally, sending this object to the reducer
                 this.props.setSelectedStream(OnlineChannel);
             }
         }).catch(err => {
@@ -27,11 +40,13 @@ export default class ChannelViewer extends Component {
         });
     }
     
+    // Runs for offline channels
     getOfflineStreamByChannelName(channel) {
         axios.get(`https://api.twitch.tv/kraken/channels/${channel}/?client_id=${process.env.REACT_APP_TWITCH_CLIENT_ID}`)
         .then(res => {
             // Transforming object to match the style of an online channel
             const OfflineChannel = Object.assign({}, { stream: null, channel: res.data });
+            // Sending object to reducer
             this.props.setSelectedStream(OfflineChannel);
         }).catch(err => {
             console.error(err)
