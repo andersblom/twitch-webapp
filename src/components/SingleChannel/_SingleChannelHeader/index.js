b2ba07f46811966341e44e7e8cab507b73766b76
Anders Blom <anders@andersblom.dk>
2017-11-27 15:20:00 -0500

    properly positioning the header+btn in SingleChannelHeader

--- src/components/SingleChannel/_SingleChannelHeader/index.js
+++ src/components/SingleChannel/_SingleChannelHeader/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import RenderSvg from '../../RenderSvg';
 
+import './singlechannelheader.css';
+
 const SingleChannelHeader = ({ channel, showChat, toggleChat }) => {
     return (
-        <div>
-            <div>{channel.name}</div>
+        <div className="singleChannelHeader__container">
+            <h1 className="singleChannelHeader__title">{channel.name}</h1>
             <button onClick={() => toggleChat()}>
                 {showChat 
                     ? "Hide chat"
