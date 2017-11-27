ab52ff2c549457b114a793706e16e4a8d62a4d44
Anders Blom <anders@andersblom.dk>
2017-11-27 09:58:17 -0500

    Escaped the error if channel is undefined problem, for now

--- src/components/SingleChannel/_ViewChannelStream/index.js
+++ src/components/SingleChannel/_ViewChannelStream/index.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 import ReactPlayer from 'react-player'
 
+import Loading from '../../Loading';
+
 export default class _ViewChannelStream extends Component {
+    componentDidMount() {
+        this.initStream();
+    }
     componentWillReceiveProps() {
         this.initStream();
     }
@@ -15,19 +20,33 @@ export default class _ViewChannelStream extends Component {
     render() {
         // console.log(channel, stream); 
         const { channel, stream } = this.props;
-        if (stream === null) {
-            return (
-                <div>Channel offline</div>
-            )
+        
+        if (channel !== undefined) {
+            if (stream === null) {
+                return (
+                    <div>Channel offline</div>
+                )
+            } else {
+                return(
+                    <div>
+                        <ReactPlayer 
+                            url={`https://www.twitch.tv/${channel.name}`}
+                            width="100%" 
+                            height="80vh" 
+                        />
+                        <iframe frameborder="0" 
+                            scrolling="no" 
+                            id="chat_embed" 
+                            src={`http://www.twitch.tv/${channel.name}/chat`}
+                            height="500" 
+                            width="350">
+                        </iframe>
+                    </div>
+                )
+            }
         } else {
-            return(
-                <div>
-                    <ReactPlayer 
-                        url={`https://www.twitch.tv/imaqtpie`}
-                        width="100%" 
-                        height="80vh" 
-                    />
-                </div>
+            return (
+                <Loading />
             )
         }
     }
