11cbe2329514325d7323dde483d1a28eaf04a665
Anders Blom <anders@andersblom.dk>
2017-11-30 17:17:20 -0500

    sidebar/header icon changes passed as props to RenderSvg

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,8 +11,8 @@ const Header = () => (
     <header>
         <Link to="/" className="logo"><img src={TwitchGlitchWhite} alt="Twitch logo" /></Link>
         <nav>
-            <Link to="/" className="header__nav__link"><RenderSvg iconName="zap" /></Link>
-            <Link to="/games" className="header__nav__link"><RenderSvg iconName="headphones" /></Link>
+            <Link to="/" className="header__nav__link"><RenderSvg iconName="zap" strokeWidth="1.3" stroke="var(--text_gray)" /></Link>
+            <Link to="/games" className="header__nav__link"><RenderSvg iconName="headphones" strokeWidth="1.3" stroke="var(--text_gray)" /></Link>
             <Link to="/user" className="header__nav__link--user"><img src="https://static-cdn.jtvnw.net/user-default-pictures/cd618d3e-f14d-4960-b7cf-094231b04735-profile_image-70x70.jpg" alt="User" /></Link>
         </nav>
     </header>
