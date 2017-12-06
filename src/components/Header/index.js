191fb90dbac0a833edde2f1326f0014907dca241
Anders Blom <anders@andersblom.dk>
2017-12-06 13:13:57 -0500

    Twtich Oauth implementation for user Auth. + deconstructing the url param response from the Twitch API. It's messy for now but it works, until I'll get a node implementation up and running to take care of this.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,9 +11,9 @@ const Header = () => (
     <header>
         <Link to="/" className="logo"><img src={TwitchGlitchWhite} alt="Twitch logo" /></Link>
         <nav>
-            <Link to="/" className="header__nav__link"><RenderSvg iconName="zap" strokeWidth="1.3" stroke="var(--text_gray)" /></Link>
-            <Link to="/games" className="header__nav__link"><RenderSvg iconName="headphones" strokeWidth="1.3" stroke="var(--text_gray)" /></Link>
-            <Link to="/user" className="header__nav__link header__nav__link--user"><RenderSvg iconName="user" strokeWidth="1.3" stroke="var(--text_gray)" /></Link>
+            <Link to="/" className="header__nav__link"><RenderSvg iconName="zap" strokeWidth={1.3} stroke="var(--text_gray)" /></Link>
+            <Link to="/games" className="header__nav__link"><RenderSvg iconName="headphones" strokeWidth={1.3} stroke="var(--text_gray)" /></Link>
+            <Link to="/user" className="header__nav__link header__nav__link--user"><RenderSvg iconName="user" strokeWidth={1.3} stroke="var(--text_gray)" /></Link>
         </nav>
     </header>
 );
--- src/components/UserArea/index.js
+++ src/components/UserArea/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const UserArea = () => (
-    <div>UserArea</div>
-)
-
-export default UserArea;
+export default class UserArea extends Component {
+    componentDidMount() {
+        const paramsObject = {};
+        let params = document.location.hash;
+        params = params.slice(1);
+        params = params.split("&");
+        
+        params.forEach(param => {
+            let splitParam = param.split("=");
+            paramsObject[splitParam[0]] = splitParam[1]
+        })
+        console.log(paramsObject);
+    }
+    render () {
+        return (
+            <div>
+                <h1>User area</h1>
+                <Link to="/user/auth">Log in using Twitch</Link>
+            </div>
+        )
+    }
+}
