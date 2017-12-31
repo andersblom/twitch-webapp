1a6b40e6e74fcdbe520c907f52680400c26de2bb
Anders Blom <anders@andersblom.dk>
2017-12-30 18:42:46 -0600

    Copy for loginWarning @ Login component

--- src/components/UserArea/Login/index.js
+++ src/components/UserArea/Login/index.js
@@ -12,7 +12,9 @@ const Login = () => (
                 <img src={TwitchWhiteTextLogo} alt="Twitch text logo" /> 
             </div>
             <div className="login__panel__textWarning">
-                ...
+                <p>To log in with your Twitch account, you will be redirected to Twitch's website 
+                where you must authorize this application to access your information.</p>
+                <p>No worries, though. We only do so to access what streams you follow, so we can pull those up for you.</p>
             </div>
             <Link className="login__btn primary" to="/login/auth">Log in using Twitch</Link>
         </div>
