f2baea06bf2d8918868af856b8143fe10fe79661
Anders Blom <anders@andersblom.dk>
2017-12-30 18:40:50 -0600

    mockup for loginpanel + header with logo

--- src/components/UserArea/Login/index.js
+++ src/components/UserArea/Login/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import TwitchWhiteTextLogo from './Twitch_White_RGB.png';
+
+import './login.css';
+
 const Login = () => (
-    <div><Link to="/login/auth">Log in using Twitch</Link></div>
+    <div className="login__container">
+        <div className="login__panel">
+            <div className="login__panel__header">
+                <img src={TwitchWhiteTextLogo} alt="Twitch text logo" /> 
+            </div>
+            <div className="login__panel__textWarning">
+                ...
+            </div>
+            <Link className="login__btn primary" to="/login/auth">Log in using Twitch</Link>
+        </div>
+    </div>
 )
 
 export default Login;
