183597a50180a4de820dfcaa34a2026a7ab4095e
Anders Blom <anders@andersblom.dk>
2018-01-18 19:45:37 -0600

    Refactored SingleStream to use flow and to be a stateless component

--- src/components/SingleStream/index.js
+++ src/components/SingleStream/index.js
@@ -1,56 +1,54 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// @flow
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { compose, withState, withHandlers } from 'recompose';
 
 import RenderSvg from '../RenderSvg';
 
 import './singlestream.css';
 
-export default class SingleStream extends Component {
-    constructor() {
-        super();
-        this.state = {
-            hover: false,
-        }
-    }
-
-    getLiveTime(streamCreationDate) {
-        let now = new Date(Date.now());
-        let startedAt = new Date(streamCreationDate);
-        let calculatedLiveTime = Math.floor(((now - startedAt) / 1000) / 60 / 60);
-        return (calculatedLiveTime > 1) ? `${calculatedLiveTime} hours` : `${calculatedLiveTime} hour`; 
-    }
-
-    toggleHoverOut() {
-        this.setState({
-            hover: false
-        });
-    }
-
-    toggleHoverIn() {
-        this.setState({
-            hover: true
-        });
-    }
-
-    render() {
-        const { stream } = this.props;
-        return(
-            <Link to={`/watch/${stream.channel.name}`} className={"singleStream__entry" + (this.state.hover ? " singleStream__entry--hover" : "")} onMouseEnter={() => this.toggleHoverIn()} onMouseLeave={() => this.toggleHoverOut()}>
-                <div className="singleStream__entry__background" style={{ backgroundImage: `url(${stream.preview.large})`}}></div>
-                <div className="singleStream__entry__overlay"></div>
-                <div className="singleStream__entry__info">
-                    <div className="singleStream__entry__info__name">{stream.channel.name}</div>
-                    <div className="singleStream__entry__info__hoverInfo">
-                        <div className="singleStream__entry__info__game"><RenderSvg iconName="video" />{stream.channel.game}</div>
-                        <div className="singleStream__entry__info__viewers"><RenderSvg iconName="tv" />{stream.viewers.toLocaleString()}</div>
-                    </div>
-                </div>
-            </Link>
-        );
-    }
+const getLiveTime = (streamCreationDate) => {
+    let now = new Date(Date.now());
+    let startedAt = new Date(streamCreationDate);
+    let calculatedLiveTime = Math.floor(((now - startedAt) / 1000) / 60 / 60);
+    return (calculatedLiveTime > 1) ? `${calculatedLiveTime} hours` : `${calculatedLiveTime} hour`; 
 }
 
-SingleStream.propTypes = {
-    stream: PropTypes.object.isRequired,
-}
+const SingleStream = (
+    { 
+        stream, 
+        isHovered, 
+        onHoverIn: hoverIn, 
+        onHoverOut: hoverOut, 
+    }:{
+        stream: {
+            channel: Object,
+            _id: String,
+            preview: Object,
+            viewers: String
+        },
+        isHovered: Boolean,
+        onHoverIn: Function,
+        onHoverOut: Function,
+    }
+) => (
+    <Link to={`/watch/${stream.channel.name}`} className={"singleStream__entry" + (isHovered ? " singleStream__entry--hover" : "")} onMouseEnter={hoverIn} onMouseLeave={hoverOut}>
+        <div className="singleStream__entry__background" style={{ backgroundImage: `url(${stream.preview.large})`}}></div>
+        <div className="singleStream__entry__overlay"></div>
+        <div className="singleStream__entry__info">
+            <div className="singleStream__entry__info__name">{stream.channel.name}</div>
+            <div className="singleStream__entry__info__hoverInfo">
+                <div className="singleStream__entry__info__game"><RenderSvg iconName="video" />{stream.channel.game}</div>
+                <div className="singleStream__entry__info__viewers"><RenderSvg iconName="tv" />{stream.viewers.toLocaleString()}</div>
+            </div>
+        </div>
+    </Link>
+);
+
+export default compose(
+    withState('isHovered', 'setHover', false),
+    withHandlers({
+        onHoverIn: ({ setHover }) => () => setHover(true),
+        onHoverOut: ({ setHover }) => () => setHover(false),
+    }),
+)(SingleStream);
